3091f83607996cf7662365ca6bdd9ce1d72c41df
arjun <arjun.94.choudhry@gmail.com>
2017-12-28 06:22:00 -0600

    fitting the thompson sampling

--- thompsonSampling/thompson.py
+++ thompsonSampling/thompson.py
@@ -13,3 +13,30 @@ import matplotlib.pyplot as plt
 
 # importing dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+# implementing thompson sampling
+import random
+d = 10
+observations = len(dataset)
+ads_selected_thompson = []
+totalRewards_thompson = 0
+number_of_ones_thompson = [0]*d
+number_of_zeroes_thompson = [0]*d
+for n in range(0,observations):
+    ad = 0
+    max_randDraw = 0
+    for i in range(0,d):
+        nOnes = number_of_ones_thompson[i]
+        nZeroes = number_of_zeroes_thompson[i]
+        randDraw = random.betavariate(nOnes+1,nZeroes+1)
+        if(randDraw > max_randDraw):
+            max_randDraw = randDraw
+            ad = i
+
+    ads_selected_thompson.append(ad)
+    reward = dataset.values[n,ad]
+    if(reward == 1):
+        number_of_ones_thompson[ad] = number_of_ones_thompson[ad] + 1
+        totalRewards_thompson = totalRewards_thompson + 1
+    else:
+        number_of_zeroes_thompson[ad] = number_of_zeroes_thompson[ad] + 1
